26c3c6d1f9ffbbb5d81e338d49ef24cb6f3790b5
megwann <meganstorey.gh@gmail.com>
2016-03-15 12:16:02 -0400

    update

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,24 +1,27 @@
-$("#getClients").click(function() {
-  //will not work locally with Chrome (use Safari, FF)
-  $.getJSON("jsonDatabase/clients.json", function(data) {
+$(document).ready(function() {
 
-    var table = "<table class='table table-striped table-hover'>" +
-      "<tr><th>Name</th><th>Company</th><th>Email</th><th>Phone</th><th>Balance</th></tr>";
+    $("#getClients").on("click", function() {
+        var url = "http://megwann.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
+        $.getJSON("http://megwann.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
+            
+            var html = "<table class='table table-hover table-striped'>"
+        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+            
+            $.each(data, function(index, item){
+                
+        html += "<tr>" +
+            "<td>" + item.name + "</td>" +
+            "<td>" + item.email + "</td>" +
+            "<td>" + item.company + "</td>" +
+            "</tr>";
+            })
+            
+            html += "</table>";
+            $("#data").append(html);
 
-    $.each(data, function(index, item) {
-      table += "<tr>"+
-      "<td>" + item.name + "</td>" +
-        "<td>" + item.company + "</td>" +
-        "<td>" + item.email + "</td>" +
-        "<td>" + item.phone + "</td>" +
-        "<td>" + item.balance + "</td>"+
-        "</tr>";
-    });
+            //alert(data);
+            //console.dir(data);
 
-    table += "</table>";
-    $("#data").append(table);
-
-  });//getJSON
-});//click
-
-//to look at a JSON file clearly copy teh contents of clients.json into http://www.jsoneditoronline.org/
+        });//getJSON
+    });//click
+});//function
