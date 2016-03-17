001d4c322f85c4207d8f62435469295ed66944ef
megwann <meganstorey.gh@gmail.com>
2016-03-17 17:07:19 -0400

    ajax

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,27 +1,30 @@
 $(document).ready(function() {
 
     $("#getClients").on("click", function() {
+
         var url = "http://megwann.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
-        $.getJSON("http://megwann.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function(data) {
-            
-            var html = "<table class='table table-hover table-striped'>"
-        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
-            
-            $.each(data, function(index, item){
-                
-        html += "<tr>" +
-            "<td>" + item.name + "</td>" +
-            "<td>" + item.email + "</td>" +
-            "<td>" + item.company + "</td>" +
-            "</tr>";
+
+        $.getJSON(url, function(data) {
+            var html = "<table class='table table-hover table-striped'>" +
+              "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+
+            $.each(data, function(index, item) {
+
+              html += "<tr>" +
+                "<td>" + item.name + "</td>" +
+                "<td>" + item.email + "</td>" +
+                "<td>" + item.company + "</td>" +
+                "</tr>";
             })
-            
+
             html += "</table>";
             $("#data").append(html);
-
             //alert(data);
             //console.dir(data);
 
-        });//getJSON
-    });//click
-});//function
+          }) //getJSON
+
+      }) //click
+
+  }) //ready
+
