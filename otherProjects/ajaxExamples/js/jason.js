dc9d36a744087f9289acb5bf9f7d86a168f1e057
megwann <meganstorey.gh@gmail.com>
2016-03-22 09:51:50 -0400

    jason

--- otherProjects/ajaxExamples/js/jason.js
+++ otherProjects/ajaxExamples/js/jason.js
@@ -3,6 +3,45 @@ $(document).ready(function(){
    $.getJSON("jsonDatabase/jason.json",function(data){ 
     
     console.dir(data);
+       
+    var html="";   
+       
+    $.each(data,funcation(index, item){
+           
+           html+='<div class="cold-md-4">'+ 
+                '<div class="jasonName"> '+item.name+' </div>+
+                '<div class="jasonGender"> '+item.gender+' </div>'+
+                '<div class="jasonJob">'+item.job+' </div>'+
+                '<img src="'+item.image+'"/>';
+          
+           html+='</div>';
+           
+           })//each Jason
+    
+    $("#jasonData").append(html);
     
 })
 })
+
+
+/* 
+//1 per Jason
+<div class="cold-md-4"> 
+    <div class="jasonName"> </div>
+    <div class="jasonGender"> </div>
+    <div class="jasonJob"> </div> 
+    <img src=""/> 
+        <div class="commentsContainer">
+            //one per comment 
+            
+            <div class="renterName"></div>
+             <div class="renterLocation"> </div>
+              <div class="renterStars"> </div> 
+              // 5 stars, some full some emty 
+              </div> // end stars 
+              </div>  //end commentt contairner 
+              </div> 
+              //end jason 
+              
+    
+*/
