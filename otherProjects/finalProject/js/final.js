d47834c9cfa665e13e2218172351ebce1b6eb64d
megwann <meganstorey.gh@gmail.com>
2016-04-11 13:12:37 -0400

    hp

--- otherProjects/finalProject/js/final.js
+++ otherProjects/finalProject/js/final.js
@@ -36,6 +36,18 @@ function getPartial(partial) {
      
        //ajax ++++++++++++++++++++ GET VIEW PAGE ++++++++++++++++++++++++
      
+    }else if (partial =="harryPage"){
+    
+        
+        
+     $.get("partials/harry.html", function (data){
+     
+     $("#pageContent").html(data);
+         
+         
+         
+     })
+    
     }else if (partial =="viewPage"){
                 
         //ajax get view.html
@@ -126,116 +138,57 @@ $("#pageContent").html(html);
          
          
          
-    }else if (partial =="orderPage"){
-    
-          //ajax get order.html
-        
-     $.get("partials/order.html", function (data){
-     
-     $("#pageContent").html(data);
-          $('.carousel').carousel()
-          
-             $('#startRentDate, #endRentDate').datepicker({});
+    }else if (partial == "orderPage") { //ajax get order.html
+                $.get("partials/order.html", function(data) {
 
-       $("#myButton").on("mouseenter", function() {
-      $("#log").append("<br>Button mouseenter");
-      $(this).text("Order Now!");
-    })
-    .on("mouseleave", function() {
-      $("#log").append("<br>Button mouseleave");
-      $(this).text("Order Now!");
-    });
+                        $("#pageContent").html(data);
+
+                      
+                        $("#submitButton").on("click", function() {
+
+                                //get all empty inputs and select
+                                //add error class to div container
+                                $("input, select").filter(function() {
+                                    return !this.value;
+                                }).closest("div").addClass("has-error");
+
+                                //remove error class for non empty ones
+                                $("input, select").filter(function() {
+                                    return this.value; //removed !
+                                }).closest("div").removeClass("has-error");
+
+                                var errors = $(".has-error");
+
+                                if (errors.length < 1) {
+                                    //alert("no errors");
+                                    sendConfirmation();
+                                }
+
+                            }) //click
+                    }) //get
+            }
+            $("#pageContent").fadeIn();
+
+        }
+
+        function sendConfirmation() {
+            //make an object to record data for database;
+            var order = {};
+            //get all teh jquery objects
+            var formData = $("input, select");
+            //for each jquery object
+            formData.each(function() {
+                var id = $(this).attr("id");//get the id of the element
+                order[id] = $(this).val();//set the field and the value
+            })
+
+            alert("Sending to database " + JSON.stringify(order));
+            $("#successMsg").html("You're A Wizard!<br/><br/>" + " Thank You for Ordering" + 
+              "<img id='dh' src='images/DH.png'>");
 
 
-  //change the backgrund color on focus, blue
-  $("#mySingleLineText").on("focus", function() {
-      $("#log").append("<br>input focus");
-      $(this).css("background-color", "#F7F8E0");
-    })
-    .on("blur", function() {
-      $("#log").append("<br>input blur");
-      $(this).css("background-color", "#FFF");
-    });
 
-  //give the user a message about their selection
-  $("#mySelect").on("change", function() {
 
-    var val = $(this).val();
-    $("#log").append("<br>select change");
-    $("#mySelectMessage").html(val + "");
-
-  });
-
-  //user clicks the button
-  $("#myButton").on("click", function() {
-
-    $("#log").append("<br>User clicked the button");
-
-    var userOrder = {};
-
-    userOrder.myInput = $("#mySingleLineText").val();
-    userOrder.myTextarea = $("#myTextarea").val();
-    userOrder.mySelect = $("#mySelect").val();
-    userOrder.myRadio = $("[name='gender']:checked").val();
-    userOrder.myCheckValues = [];
-
-    $("[name='vehicle']:checked").each(function() {
-      userOrder.myCheckValues.push($(this).val());
-    });
-
-    $("#log").append("<br>Value of input is: " + userOrder.myInput);
-    $("#log").append("<br>Value of textarea is: " + userOrder.myTextarea);
-    $("#log").append("<br>Value of select is: " + userOrder.mySelect);
-    $("#log").append("<br>Value of radio button is: " + userOrder.myRadio);
-    $("#log").append("<br>Value of checks is: " + userOrder.myCheckValues.join());
-    $("#log").append("<br><br>Value of userOrder is: " + JSON.stringify(userOrder));
-
-    /*
-        var myInput = $("#mySingleLineText").val();
-        var myTextarea = $("#myTextarea").val();
-        var mySelect = $("#mySelect").val();
-        var myRadio = $("[name='gender']:checked").val();
-        var myCheckValues = [];
-        //each is a jquery loop for objects/arrays
-        //each thing that is selected, do the function
-        //"this" is the element we are currently looking at
-        $("[name='vehicle']:checked").each(function() {
-          myCheckValues.push($(this).val());
-        });
-        $("#log").append("<br>User clicked the button");
-        $("#log").append("<br>Value of input is: " + myInput);
-        $("#log").append("<br>Value of textarea is: " + myTextarea);
-        $("#log").append("<br>Value of select is: " + mySelect);
-        $("#log").append("<br>Value of radio button is: " + myRadio);
-        $("#log").append("<br>Value of checks is: " + myCheckValues.join());
-    */
-  })
-  
-         
-         
-     })   
-        
-    }else if (partial =="harryPage"){
-    
-        
-        
-     $.get("partials/harry.html", function (data){
-     
-     $("#pageContent").html(data);
-         
-         
-         
-     })}
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
       
          //ajax ++++++++++++++++++++ END ORDER PAGE ++++++++++++++++++++++++    
 
